8455ef01e78545885d8dd5d8e2d80788b5d4f0b9
Tobias Krais <tk@design-to-use.de>
2017-01-22 16:47:58 +0100

    1.1.0 Color setting bug fixed

--- pages/help/changelog.php
+++ pages/help/changelog.php
@@ -1,5 +1,9 @@
 <fieldset>
 	<legend>D2U Helper Changelog</legend>
+	<p>1.1.1</p>
+	<ul>
+		<li>Farbbug bei weißem Menühintergrund behoben</li>
+	</ul>
 	<p>1.1.0</p>
 	<ul>
 		<li>Templates hinzugefügt</li>
