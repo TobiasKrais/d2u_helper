0146f6e06b439c64996a3009ba3122e1e6016792
Tobias Krais <tk@design-to-use.de>
2017-02-03 22:55:49 +0100

    Bugfix: upgrade from older version failed, because class ist not available

--- update.php
+++ update.php
@@ -1,8 +1,12 @@
 <?php
 // Update modules
-$d2u_module_manager = new D2UModuleManager(D2UModuleManager::getD2UHelperModules());
-$d2u_module_manager->autoupdate();
+if(class_exists(D2UModuleManager)) {
+	$d2u_module_manager = new D2UModuleManager(D2UModuleManager::getD2UHelperModules());
+	$d2u_module_manager->autoupdate();
+}
 
 // Update templates
-$d2u_template_manager = new D2UTemplateManager(D2UTemplateManager::getD2UHelperTemplates());
-$d2u_template_manager->autoupdate();
+if(class_exists(D2UTemplateManager)) {
+	$d2u_template_manager = new D2UTemplateManager(D2UTemplateManager::getD2UHelperTemplates());
+	$d2u_template_manager->autoupdate();
+}
