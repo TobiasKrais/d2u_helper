17a507164f4cf866877a1abc92e5ccb1a534cc1b
Tobias Krais <tk@design-to-use.de>
2017-08-07 23:47:26 +0200

    New Module range

--- lib/d2u_module_manager.php
+++ lib/d2u_module_manager.php
@@ -173,6 +173,7 @@ class D2UModuleManager {
 		$modules[] = new D2UModule("10-3",
 			"Box mit Download",
 			1);
+		// 30-x reserved for D2U Videos
 		// 40-x reserved for D2U News
 		// 50-x reserved for D2U References
 		// 60-x reserved for D2U Guestbook
