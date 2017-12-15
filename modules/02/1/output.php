56de84174593334546962862988161b7bf58735b
Tobias Krais <tk@design-to-use.de>
2017-12-15 23:11:16 +0100

    Better headline module

--- modules/02/1/output.php
+++ modules/02/1/output.php
@@ -3,7 +3,14 @@
 	if($cols == "") {
 		$cols = 8;
 	}
+
+	$offset_lg_cols = intval("REX_VALUE[17]");
+	$offset_lg = "";
+	if($offset_lg_cols > 0) {
+		$offset_lg = " mr-lg-auto ml-lg-auto ";
+	}
+
 ?>
-<div class="col-sm-12 col-md-<?php echo $cols; ?>">
+<div class="col-sm-12 col-md-<?php echo $cols . $offset_lg; ?>">
 	<REX_VALUE[2]>REX_VALUE[1]</REX_VALUE[2]>
 </div>
--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -10,7 +10,8 @@
 		</ul>
 		<p>1.3.1 (NEXT)</p>
 		<ul>
-			<li>Modul 03-2 verbessert.</li>
+			<li>Modul 02-1 (Überschrift) verbessert.</li>
+			<li>Modul 03-2 (Ekko Lightbox) verbessert.</li>
 			<li>Für Template 02-1 kann die Navigation in den Einstellungen auch oberhalb des Headerbildes gesetzt werden.</li>
 			<li>Update bootstrap 4 beta2.</li>
 			<li>Update Ekko Lightbox to version 5.3.</li>
