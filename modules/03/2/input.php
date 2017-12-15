07f31763c598ae9da7977fe74c6af68b9b23ad6d
Tobias Krais <tk@design-to-use.de>
2017-12-15 23:08:01 +0100

    Better gallery.

--- modules/03/2/input.php
+++ modules/03/2/input.php
@@ -1,15 +1,75 @@
 <div class="row">
 	<div class="col-xs-4">
-		Breite des Blocks:
+		Breite des Blocks auf Smartphones:
 	</div>
 	<div class="col-xs-8">
 		<select name="REX_INPUT_VALUE[20]" >
 		<?php
-		$values = [12=>"12 von 12 Spalten (ganze Breite)", 6=>"6 von 12 Spalten", 4=>"4 von 12 Spalten", 3=>"3 von 12 Spalten"];
+		$values = [12=>"12 von 12 Spalten (ganze Breite)", 8=>"8 von 12 Spalten", 6=>"6 von 12 Spalten", 4=>"4 von 12 Spalten", 3=>"3 von 12 Spalten"];
 		foreach($values as $key => $value) {
 			echo '<option value="'. $key .'" ';
 	
 			if ("REX_VALUE[20]" == $key) {
+				echo 'selected="selected" ';
+			}
+			echo '>'. $value .'</option>';
+		}
+		?>
+		</select>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-4">
+		Breite des Blocks auf Tablets:
+	</div>
+	<div class="col-xs-8">
+		<select name="REX_INPUT_VALUE[19]" >
+		<?php
+		$values = [12=>"12 von 12 Spalten (ganze Breite)", 8=>"8 von 12 Spalten", 6=>"6 von 12 Spalten", 4=>"4 von 12 Spalten", 3=>"3 von 12 Spalten"];
+		foreach($values as $key => $value) {
+			echo '<option value="'. $key .'" ';
+	
+			if ("REX_VALUE[19]" == $key) {
+				echo 'selected="selected" ';
+			}
+			echo '>'. $value .'</option>';
+		}
+		?>
+		</select>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-4">
+		Breite des Blocks auf größeren Geräten:
+	</div>
+	<div class="col-xs-8">
+		<select name="REX_INPUT_VALUE[18]" >
+		<?php
+		$values = [12=>"12 von 12 Spalten (ganze Breite)", 8=>"8 von 12 Spalten", 6=>"6 von 12 Spalten", 4=>"4 von 12 Spalten", 3=>"3 von 12 Spalten"];
+		foreach($values as $key => $value) {
+			echo '<option value="'. $key .'" ';
+	
+			if ("REX_VALUE[18]" == $key) {
+				echo 'selected="selected" ';
+			}
+			echo '>'. $value .'</option>';
+		}
+		?>
+		</select>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-4">
+		Offset (Seitenabstand) auf größeren Geräten:
+	</div>
+	<div class="col-xs-8">
+		<select name="REX_INPUT_VALUE[17]" >
+		<?php
+		$values = array(0=>"Kein Offset", 1=>"Offset");
+		foreach($values as $key => $value) {
+			echo '<option value="'. $key .'" ';
+	
+			if ("REX_VALUE[17]" == $key) {
 				echo 'selected="selected" ';
 			}
 			echo '>'. $value .'</option>';
--- modules/03/2/output.php
+++ modules/03/2/output.php
@@ -1,27 +1,59 @@
 <?php
-	$cols = "REX_VALUE[20]";
-	if($cols == "") {
-		$cols = 8;
+	$cols_sm = "REX_VALUE[20]";
+	if($cols_sm == "") {
+		$cols_sm = 8;
 	}
-	$pics_cols = 4;
-	if($cols >= 10) {
-		$pics_cols = 3;
+	$cols_md = "REX_VALUE[19]";
+	if($cols_md == "") {
+		$cols_md = $cols_sm;
+		$cols_sm = 12; // Backward compatibility
 	}
-	else if ($cols <= 6) {
-		$pics_cols = 6;
+	$cols_lg = "REX_VALUE[18]";
+	if($cols_lg == "") {
+		$cols_lg = $cols_md;
 	}
+	$offset_lg_cols = intval("REX_VALUE[17]");
+	$offset_lg = "";
+	if($offset_lg_cols > 0) {
+		$offset_lg = " mr-lg-auto ml-lg-auto ";
+	}
+	
+	// Number pics per row
+	$pics_cols = 'col-6';
+	if($cols_sm == 12) {
+		$pics_cols .= ' col-sm-4';
+	}
+	
+	if($cols_md == 12) {
+		$pics_cols .= ' col-md-3';
+	}
+	elseif($cols_md == 8) {
+		$pics_cols .= ' col-md-4';
+	}
+
+	if($cols_lg == 12) {
+		$pics_cols .= ' col-lg-2';
+	}
+	elseif($cols_lg == 8) {
+		$pics_cols .= ' col-lg-3';
+	}
+	elseif($cols_lg == 6) {
+		$pics_cols .= ' col-lg-4';
+	}
+
 	$type_thumb = "d2u_helper_gallery_thumb";
 	$type_detail = "d2u_helper_gallery_detail";
 	$pics = preg_grep('/^\s*$/s', explode(",", REX_MEDIALIST[1]), PREG_GREP_INVERT);
 	
 	$lightbox_id = rand();
+
 ?>
-<div class="col-12 col-md-<?php echo $cols; ?>">
+<div class="col-12 col-sm-<?php echo $cols_sm; ?> col-md-<?php echo $cols_md; ?> col-lg-<?php echo $cols_lg . $offset_lg; ?>">
 	<div class="row">
 		<?php
 			foreach($pics as $pic) {
 				$media = rex_media::get($pic);
-				print '<a href="index.php?rex_media_type='. $type_detail .'&rex_media_file='. $pic .'" data-toggle="lightbox'. $lightbox_id .'" data-gallery="example-gallery'. $lightbox_id .'" class="col-4 col-lg-'. $pics_cols .'"';
+				print '<a href="index.php?rex_media_type='. $type_detail .'&rex_media_file='. $pic .'" data-toggle="lightbox'. $lightbox_id .'" data-gallery="example-gallery'. $lightbox_id .'" class="'. $pics_cols .'"';
 				if($media instanceof rex_media) {
 					print ' data-title="'. $media->getValue('title') .'"';
 				}
