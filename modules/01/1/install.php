31a04ecd097f25e64e685ba138bb76b33ceb21bc
Tobias Krais <tk@design-to-use.de>
2018-03-20 00:00:48 +0100

    Multiple Editors allowed: https://github.com/TobiasKrais/d2u_news/issues/1

--- modules/01/1/install.php
+++ modules/01/1/install.php
@@ -1,5 +1,8 @@
 <?php
-if(!rex_addon::get('redactor2')->isAvailable() && !rex_addon::get('tinymce4')->isAvailable()) {
+if(!rex_addon::get('ckeditor')->isAvailable()
+		&& !rex_addon::get('markitup')->isAvailable()
+		&& !rex_addon::get('redactor2')->isAvailable()
+		&& !rex_addon::get('tinymce4')->isAvailable()) {
 	print rex_view::error(rex_i18n::msg('d2u_helper_modules_install_wysiwyg'));
 	return FALSE;
 }
