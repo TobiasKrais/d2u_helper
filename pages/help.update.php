bdafbe04bddc5dc6c1acede105c7b050d48bfbdd
Tobias Krais <tk@design-to-use.de>
2018-04-19 22:59:34 +0200

    Prepare 1.4.2

--- pages/help.update.php
+++ pages/help.update.php
@@ -1,6 +1,13 @@
 <div class="panel panel-edit">
 	<header class="panel-heading"><div class="panel-title">Updatehinweise</div></header>
 	<div class="panel-body">
+		<p>1.4.1:</p>
+		<ul>
+			<li>Bitte den Artikel der Datenschutzerklärung in den Einstellungen
+				hinterlegen. <a href="https://dsgvo-muster-datenschutzerklaerung.dg-datenschutz.de/">
+				Ein kostenloser Datenschutzerklärungsgenerator ist</a> hier zu
+				finden.</li>
+		</ul>
 		<p>1.2.3:</p>
 		<ul>
 			<li>Falls das Template "00-1 Big Header Template" installiert ist,
