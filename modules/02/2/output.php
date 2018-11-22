e6d6ad84d07fddb436e2541d9f904196a663f934
Tobias Krais <tk@design-to-use.de>
2018-11-22 22:14:24 +0100

    bugfix multiple use

--- modules/02/2/output.php
+++ modules/02/2/output.php
@@ -19,9 +19,9 @@
 	
 	$id = rand();
 	
-	print '<div class="col-12 col-sm-'. $cols_sm .' col-md-'. $cols_md .' col-lg-'. $cols_lg . $offset_lg .'">';
+	print '<div class="col-12 col-sm-'. $cols_sm .' col-md-'. $cols_md .' col-lg-'. $cols_lg . $offset_lg .' toggle_spacer">';
 
-	print '<REX_VALUE[2] onClick="toggle_view()">REX_VALUE[1]<span class="fa-icon h_toggle icon_up" id="'. $id .'_arrow"></span></REX_VALUE[2]>';
+	print '<REX_VALUE[2] onClick="toggle_view_'. $id .'()">REX_VALUE[1]<span class="fa-icon h_toggle icon_up" id="'. $id .'_arrow"></span></REX_VALUE[2]>';
 	
 	print '<div class="wysiwyg_content" id="'. $id .'_text">';
 	if ('REX_VALUE[id=3 isset=3]') {
@@ -40,7 +40,7 @@
 	print '</div>';
 ?>
 <script>
-	function toggle_view() {
+	function toggle_view_<?php echo $id; ?>() {
 		$("#<?php echo $id; ?>_text").slideToggle();
 		$("#<?php echo $id; ?>_arrow").toggleClass("icon_down");
 		$("#<?php echo $id; ?>_arrow").toggleClass("icon_up");
@@ -48,6 +48,6 @@
 
 	// Hide on document load
 	$(document).ready(function() {
-		toggle_view();
+		toggle_view_<?php echo $id; ?>();
 	});
 </script>
