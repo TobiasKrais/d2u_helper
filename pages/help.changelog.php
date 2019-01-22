3ee51132a43cb0aeb6fe8dcb06b9553c6673453f
Tobias Krais <tk@design-to-use.de>
2019-01-22 16:58:24 +0100

    1.5.6 prepared

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -1,6 +1,10 @@
 <div class="panel panel-edit">
 	<header class="panel-heading"><div class="panel-title">D2U Helper Changelog</div></header>
 	<div class="panel-body">
+		<p>1.5.6-DEV</p>
+		<ul>
+			<li>...</li>
+		</ul>
 		<p>1.5.5</p>
 		<ul>
 			<li>JS Methode zum Ausblenden von Sprachübersetzungen in kommenden Versionen von D2U Addons hinzugefügt.</li>
