3f08a0f55e4b03cb5597dcd78c4276dd3662ff63
Tobias Krais <tk@design-to-use.de>
2019-04-01 18:25:05 +0200

    prepare 1.5.8

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -1,6 +1,10 @@
 <div class="panel panel-edit">
 	<header class="panel-heading"><div class="panel-title">D2U Helper Changelog</div></header>
 	<div class="panel-body">
+		<p>1.5.8-DEV</p>
+		<ul>
+			<li>...</li>
+		</ul>
 		<p>1.5.7</p>
 		<ul>
 			<li>Verbesserung Templates: Mobile Navi wurde von Google manchmals als nicht mobiloptimiert bemängelt.</li>
