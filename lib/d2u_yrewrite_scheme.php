81a5720214f65c70ac8c6fa7fba63f54f320d84d
Tobias Krais <tk@design-to-use.de>
2019-08-09 08:06:43 +0200

    lowercase urlencoded urls

--- lib/d2u_yrewrite_scheme.php
+++ lib/d2u_yrewrite_scheme.php
@@ -20,6 +20,11 @@ class d2u_yrewrite_scheme extends rex_yrewrite_scheme {
 		
 		// In case settings require URL encode or normalizing the standard way failed
 		if(rex_config::get('d2u_helper', 'rewrite_scheme_clang_'. $clang, 'standard') == 'urlencode' || ($parent_string == "" || $parent_string == "-")) {
+			$string = str_replace(
+				['й'],
+				['и'],
+				mb_strtolower($string)
+			);
 			return preg_replace('/[+-]+/', '-', $string);
 		}
 
