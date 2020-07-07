b2dc1fdf1bb4312170e88346158086ecfa70ac25
Tobias Krais <tk@design-to-use.de>
2020-07-07 15:51:50 +0200

    prepare 1.7.1

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -1,6 +1,10 @@
 <div class="panel panel-edit">
 	<header class="panel-heading"><div class="panel-title">D2U Helper Changelog</div></header>
 	<div class="panel-body">
+		<p>1.7.1-DEV</p>
+		<ul>
+			<li>...</li>
+		</ul>
 		<p>1.7.0</p>
 		<ul>
 			<li>TinyMCE 5 als Lieblingseditor hinzugefügt.</li>
