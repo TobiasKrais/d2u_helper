b1d75156f7c331caacbbd43ca7c17cfd0f0cff47
Tobias Krais <tk@design-to-use.de>
2021-01-04 21:30:13 +0100

    prepare 1.8.3

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -1,6 +1,10 @@
 <div class="panel panel-edit">
 	<header class="panel-heading"><div class="panel-title">D2U Helper Changelog</div></header>
 	<div class="panel-body">
+		<p>1.8.3-DEV</p>
+		<ul>
+			<li>...</li>
+		</ul>
 		<p>1.8.2</p>
 		<ul>
 			<li>CSS Fehler behoben der Detailbilder im Modul 03-2 "Bildergalerie Ekko Lightbox" zu klein gemacht hatte.</li>
