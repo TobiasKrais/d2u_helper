74cb571aa6d737df2f892dbbe52f552560e9bb09
Tobias Krais <tk@design-to-use.de>
2021-08-02 11:05:41 +0200

    add faq

--- pages/modules.php
+++ pages/modules.php
@@ -19,6 +19,15 @@ $d2u_module_manager->showManagerList();
 	der installierten Module automatisch eingebunden werden. Einige Module
 	funktionieren auch ohne Bootstrap, andere - wie Ekko Lightbox - nicht.</p>
 <ul>
+	<li><b>Warum funktioniert mein Modul nicht korrekt?</b><br>
+		Beispiel: wenn man beim YouTube Modul auf Play drückt, wird das Viedo nicht geladen.<br>
+		Dann wurde das JavaScript oder Stylesheet nicht korrekt geladen. Hierzu
+			muss in den Einstellungen die Option "CSS / JS der installierten Module
+			automatisch einbinden" aktiviert werden oder das sie CSS / JS Dateiinhalte
+			manuell nachgeladen werden. Wo findet man die CSS / JS Dateien? Im
+			Dateisystem im Redaxo Addon Order unter module/. Dann die Modulnummer.
+			Die CSS Datei trägt den Namen style.css und die JS Datei js.js.		
+	</li>
 	<li><b>Wie kann der in den Modulen verwendete Editor geändert werden?</b><br>
 		Um den Lieblingseditor für die Module und und die D2U Addons
 			einzustellen einfach in den Einstellungen dieses Addons den gewünschten
