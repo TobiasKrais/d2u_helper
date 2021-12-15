5ca425787cba809c4c656a75ab0b929cf3eb1685
Tobias Krais <tk@design-to-use.de>
2021-12-15 17:19:45 +0100

    add width 10 of 12

--- modules/06/1/input.php
+++ modules/06/1/input.php
@@ -5,7 +5,7 @@
 	<div class="col-xs-8">
 		<select name="REX_INPUT_VALUE[20]" class="form-control">
 		<?php
-		$values = [12=>"12 von 12 Spalten (ganze Breite)", 10=>"10 von 12 Spalten (ganze Breite)", 8=>"8 von 12 Spalten", 6=>"6 von 12 Spalten", 4=>"4 von 12 Spalten", 3=>"3 von 12 Spalten"];
+		$values = [12=>"12 von 12 Spalten (ganze Breite)", 10=>"10 von 12 Spalten", 8=>"8 von 12 Spalten", 6=>"6 von 12 Spalten", 4=>"4 von 12 Spalten", 3=>"3 von 12 Spalten"];
 		foreach($values as $key => $value) {
 			echo '<option value="'. $key .'" ';
 	
