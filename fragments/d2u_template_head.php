3d70db58eaad2d56ae1fcadb9cf309b726b7d675
Tobias Krais <tk@design-to-use.de>
2022-06-19 13:21:49 +0200

    direct php code

--- fragments/d2u_template_head.php
+++ fragments/d2u_template_head.php
@@ -1,12 +1,12 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?php
-	print d2u_addon_frontend_helper::getMetaTags();
-	if(file_exists(rex_path::media('favicon.ico'))) {
-		print '<link rel="apple-touch-icon" href="'. rex_url::media('favicon.ico') .'">';
-		print '<link rel="icon" href="'. rex_url::media('favicon.ico') .'">';
-	}
-	if(rex_addon::get('consent_manager')->isAvailable()) {
-		print 'REX_CONSENT_MANAGER[]';
-	}
-?>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php
+		print d2u_addon_frontend_helper::getMetaTags();
+		if(file_exists(rex_path::media('favicon.ico'))) {
+			print '	<link rel="apple-touch-icon" href="'. rex_url::media('favicon.ico') .'">'. PHP_EOL;
+			print '	<link rel="icon" href="'. rex_url::media('favicon.ico') .'">'. PHP_EOL;
+		}
+		if(rex_addon::get('consent_manager')->isAvailable()) {
+			print consent_manager_frontend::getFragment(false, 'consent_manager_box_cssjs.php');
+		}
+	?>
