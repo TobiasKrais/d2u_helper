94a0c4844f0d1c17a31f230bb2a17b88dc3120e8
Tobias Krais <tk@design-to-use.de>
2026-03-03 14:52:06 +0100

    bugfix footer

--- fragments/d2u_template_bs5_footer.php
+++ fragments/d2u_template_bs5_footer.php
@@ -3,18 +3,18 @@
 $footer_type = rex_config::get('d2u_helper', 'footer_type', '');
 
 $fragment = new rex_fragment();
-if ('bs5_box' === $footer_type) {
+if ('box' === $footer_type || 'bs5_box' === $footer_type) {
     echo $fragment->parse('d2u_template_bs5_footer_box.php');
-} elseif ('bs5_box_logo' === $footer_type) {
+} elseif ('box_logo' === $footer_type || 'bs5_box_logo' === $footer_type) {
     echo $fragment->parse('d2u_template_bs5_footer_box_logo.php');
-} elseif ('bs5_links_address_contact_logo' === $footer_type) {
+} elseif ('links_address_contact_logo' === $footer_type || 'bs5_links_address_contact_logo' === $footer_type) {
     echo $fragment->parse('d2u_template_bs5_footer_links_address_contact_logo.php');
-} elseif ('bs5_simple_contact_links' === $footer_type) {
+} elseif ('simple_contact_links' === $footer_type || 'bs5_simple_contact_links' === $footer_type) {
     echo $fragment->parse('d2u_template_bs5_footer_simple_contact_links.php');
-} elseif ('bs5_links_logo_address' === $footer_type) {
+} elseif ('links_logo_address' === $footer_type || 'bs5_links_logo_address' === $footer_type) {
     echo $fragment->parse('d2u_template_bs5_footer_links_logo_address.php');
-} elseif ('bs5_links_text' === $footer_type) {
+} elseif ('links_text' === $footer_type || 'bs5_links_text' === $footer_type) {
     echo $fragment->parse('d2u_template_bs5_footer_links_text.php');
-} elseif ('bs5_text' === $footer_type) {
+} elseif ('text' === $footer_type || 'bs5_text' === $footer_type) {
     echo $fragment->parse('d2u_template_bs5_footer_text.php');
 }
