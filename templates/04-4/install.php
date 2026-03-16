73238900eaf5a91514e1885900d4f601d60ef9fc
Tobias Krais <tk@design-to-use.de>
2026-03-16 13:05:32 +0100

    remove not necessary warning

--- templates/04-4/install.php
+++ templates/04-4/install.php
@@ -15,10 +15,6 @@ if (!rex_addon::get('sprog')->isAvailable()) {
     return false;
 }
 
-if (!rex_addon::get('d2u_news')->isAvailable()) {
-    echo rex_view::warning(rex_i18n::msg('d2u_helper_templates_install_d2u_news'));
-}
-
 // Add clang_icon as Metainfo
 rex_metainfo_add_field('translate:d2u_helper_icon', 'clang_icon', 1, '', rex_metainfo_table_manager::FIELD_REX_MEDIA_WIDGET, '', 'types="gif,jpg,png,webp,svg" preview="1"');
 
